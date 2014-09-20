41e703b9516e8eb73ac9ac257857ccb650fa5f59
Krzysztof Magosa <krzysztof@magosa.pl>
2014-09-20 21:51:45 +0200

    New InputLayer class

---
+++ include/InputLayer.h
@@ -0,0 +1,29 @@
+/**
+ * Copyright 2014 Krzysztof Magosa
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef __Microbe__InputLayer__
+#define __Microbe__InputLayer__
+
+#include <iostream>
+#include "Layer.h"
+
+namespace microbe {
+    class InputLayer : public Layer {
+    public:
+        InputLayer(const int neuronsCount);
+    };
+}
+
+#endif /* defined(__Microbe__InputLayer__) */
