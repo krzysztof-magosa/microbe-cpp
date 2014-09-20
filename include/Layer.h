949d2414562381f1a761b44a2654fc39304831e4
Krzysztof Magosa <krzysztof@magosa.pl>
2014-09-20 21:44:39 +0200

    New Layer class

---
+++ include/Layer.h
@@ -0,0 +1,40 @@
+/**
+ * Copyright 2014 Krzysztof Magosa
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef __Microbe__Layer__
+#define __Microbe__Layer__
+
+#include <iostream>
+#include <vector>
+#include "Neuron.h"
+#include "BiasNeuron.h"
+
+namespace microbe {
+    class Layer {
+    public:
+        Layer(const int, Function*, const bool);
+        Layer(const int neuronsCount, Function* function) : Layer(neuronsCount, function, true) {};
+        Layer(void);
+        std::vector<Neuron*> getNeurons();
+        void run(void);
+        void initBias(void);
+        bool hasBias(void);
+        
+    protected:
+        std::vector<Neuron*> neurons;
+    };
+}
+
+#endif /* defined(__Microbe__Layer__) */
---
+++ src/Layer.cpp
@@ -0,0 +1,69 @@
+/**
+ * Copyright 2014 Krzysztof Magosa
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "Layer.h"
+
+namespace microbe {
+    Layer::Layer()
+    {
+    }
+    
+    Layer::Layer(const int neuronsCount, Function *function, const bool withBias)
+    {
+        for (int i = 1; i <= neuronsCount; ++i) {
+            Neuron *neuron = new Neuron();
+            neuron->setFunction(function);
+            neurons.push_back(neuron);
+        }
+        
+        if (withBias) {
+            neurons.push_back(new BiasNeuron());
+        }
+    }
+    
+    std::vector<Neuron*> Layer::getNeurons(void)
+    {
+        return neurons;
+    }
+    
+    void Layer::run(void)
+    {
+        std::vector<Neuron*>::iterator it;
+        
+        for (it = neurons.begin(); it != neurons.end(); ++it) {
+            Neuron &neuron = **it;
+            neuron.activate();
+        }
+    }
+    
+    void Layer::initBias(void)
+    {
+        neurons.push_back(new BiasNeuron());
+    }
+                          
+    bool Layer::hasBias()
+    {
+        std::vector<Neuron*>::iterator it;
+        for (it = neurons.begin(); it != neurons.end(); ++it) {
+            Neuron &neuron = **it;
+            
+            if (neuron.getType() == Neuron::NeuronType::Bias) {
+                return true;
+            }
+        }
+        
+        return false;
+    }
+}
