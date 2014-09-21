33cfe12350ba4326e4d2ead061778eef0c3041ca
Krzysztof Magosa <krzysztof@magosa.pl>
2014-09-21 17:42:11 +0200

    New SmartLearningRate plugin

---
+++ src/SmartLearningRate.cpp
@@ -0,0 +1,45 @@
+/**
+ * Copyright 2014 Krzysztof Magosa
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "SmartLearningRate.h"
+
+namespace microbe {
+    void SmartLearningRate::init()
+    {
+    	this->teacher->setLearningRate(1.0 / this->teacher->getLearningSets().size());
+    }
+    
+    void SmartLearningRate::preEpoch(void)
+    {
+    	lastError = teacher->getLastEpochError();
+    }
+
+    void SmartLearningRate::postEpoch(void)
+    {
+    	if (ready) {
+    		double learningRate = teacher->getLearningRate();
+
+    		if ((teacher->getLastEpochError() / lastError) > 1.04) {
+    			teacher->setLearningRate(learningRate * 0.70);
+    		}
+    		else if ((teacher->getLastEpochError() - lastError) < 0.0) {
+   				teacher->setLearningRate(learningRate * 1.02);
+    		}
+    	}
+    	else {
+    		ready = true;
+    	}
+    }
+}
