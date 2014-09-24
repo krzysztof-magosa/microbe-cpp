d5b3dff97957879a27ce5a1a15e010dde71aa42d
Krzysztof Magosa <krzysztof@magosa.pl>
2014-09-24 23:05:58 +0200

    Normalizer class with tests

---
+++ src/Normalizer.cpp
@@ -0,0 +1,43 @@
+/**
+ * Copyright 2014 Krzysztof Magosa
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "Normalizer.h"
+
+namespace microbe {
+    Normalizer::Normalizer(const double minInput, const double maxInput, const double minOutput, const double maxOutput)
+    {
+        this->minInput = minInput;
+        this->maxInput = maxInput;
+
+        this->minOutput = minOutput;
+        this->maxOutput = maxOutput;
+    }
+
+    double Normalizer::normalize(const double value)
+    {
+        return minOutput + (((value - minInput) / (maxInput - minInput)) * (maxOutput - minOutput));
+    }
+
+    std::vector<double> Normalizer::normalize(std::vector<double> values)
+    {
+        std::vector<double> result;
+
+        for (double value : values) {
+            result.push_back(normalize(value));
+        }
+
+        return result;
+    }
+}
---
+++ tests/Normalizer.cpp
@@ -0,0 +1,40 @@
+/**
+ * Copyright 2014 Krzysztof Magosa
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "gtest/gtest.h"
+#include "Normalizer.h"
+
+TEST(NormalizerTest, Normalize)
+{
+    microbe::Normalizer normalizer(0.0, 255.0, 0.0, 1.0);
+
+    EXPECT_NEAR(0.0, normalizer.normalize(0.0), 0.00001);
+    EXPECT_NEAR(0.5, normalizer.normalize(127.5), 0.00001);
+    EXPECT_NEAR(1.0, normalizer.normalize(255.0), 0.00001);
+}
+
+TEST(NormalizerTest, NormalizeVector)
+{
+    microbe::Normalizer normalizer(0.0, 255.0, 0.0, 1.0);
+
+    std::vector<double> values { 0.0, 127.5, 255.0 };
+    std::vector<double> expected { 0.0, 0.5, 1.0 };
+
+    auto processed = normalizer.normalize(values);
+
+    for (int i = 0; i < values.size(); i++) {
+        EXPECT_NEAR(expected.at(i), processed.at(i), 0.00001);
+    }
+}
